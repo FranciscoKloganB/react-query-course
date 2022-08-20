f3655a271c351cb1ba8669e82aea4d2036c8c3f9
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-08-20 11:43:31 +0100

    feat: install query client and provide it

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 import { worker } from "@uidotdev/react-query-api"
+import { QueryClient, QueryClientProvider } from "react-query"
 import { BrowserRouter } from "react-router-dom"
 
 import App from "./App"
+
+const queryClient = new QueryClient()
 
 new Promise((res) => setTimeout(res, 100))
   .then(() =>
@@ -18,11 +21,13 @@ new Promise((res) => setTimeout(res, 100))
   .then(() => {
     ReactDOM.render(
       <React.StrictMode>
-        <BrowserRouter>
-          <div className="container">
-            <App />
-          </div>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <div className="container">
+              <App />
+            </div>
+          </BrowserRouter>
+        </QueryClientProvider>
       </React.StrictMode>,
       document.getElementById("root")
     )
