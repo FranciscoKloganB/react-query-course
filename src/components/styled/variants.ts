3a620d5b8e2a34415d96be724e417947f425055c
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-08-21 19:46:41 +0100

    feat: add basic variants file

---
+++ src/components/styled/variants.ts
@@ -0,0 +1,17 @@
+export const avatarVariants = {
+  xs: "h-6 w-6",
+  sm: "h-8 w-8",
+  md: "h-10 w-10",
+  lg: "h-12 w-12",
+  xl: "h-14 w-14",
+  "2xl": "h-16 w-16"
+}
+
+export const statusOverlayVariants = {
+  xs: "h-1.5 w-1.5",
+  sm: "h-2 w-2",
+  md: "h-2.5 w-2.5",
+  lg: "h-3 w-3",
+  xl: "h-3.5 w-3.5",
+  "2xl": "h-4 w-4"
+}
