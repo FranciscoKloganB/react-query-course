8a269e43ef9bbcc9e11d1929576ab0a48a1e91c4
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-08-22 22:35:43 +0100

    feat: add LabelChip component

---
+++ src/components/ui/LabelChip.tsx
@@ -0,0 +1,22 @@
+import { useLabels } from "@/src/hooks"
+import { Chip } from "@styled"
+
+export function LabelChip({ labelName }: { labelName: string }) {
+  const labels = useLabels()
+
+  if (labels.isLoading) {
+    return null
+  }
+
+  if (labels.isError) {
+    return <Chip $color="default">{labelName}</Chip>
+  }
+
+  const label = labels.data.find((queryLabel) => queryLabel.name === labelName) ?? {
+    color: "default",
+    id: labelName,
+    name: labelName
+  }
+
+  return <Chip $color={label.color}>{label.name}</Chip>
+}
