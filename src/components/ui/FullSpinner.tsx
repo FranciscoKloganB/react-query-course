0cf88e0f4eda5f9898a1bb186a8885e44a365d43
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-08-27 09:12:20 +0100

    refactor: create app spinner abstractions

---
+++ src/components/ui/FullSpinner.tsx
@@ -0,0 +1,9 @@
+import { FaSpinner } from "react-icons/fa"
+
+export function FullSpinner() {
+  return (
+    <div className="w-full text-center">
+      <FaSpinner className="m-auto animate-spin text-white" />
+    </div>
+  )
+}
