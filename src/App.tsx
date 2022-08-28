9d224e015c7fee5d93952130e9543d01b44e2230
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-08-28 20:28:29 +0100

    fix: style App link

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Link, Route, Routes, useMatch } from "react-router-dom"
+import { Route, Routes, useMatch } from "react-router-dom"
 
 import { AppContainer, Heading } from "@components/styled"
 import AddIssue from "@pages/AddIssue"
 import Issue from "@pages/Issue"
 import Issues from "@pages/Issues"
+import { Link } from "@rrd/link"
 
 function App() {
   const isRootPath = useMatch({ path: "/", end: true })
   return (
     <AppContainer>
-      {!isRootPath ? <Link to="/">Back to Issues List</Link> : <span>&nbsp;</span>}
       <Heading className="mb-2 lg:mb-6">Issue Tracker</Heading>
+      {!isRootPath ? <Link to="/">See all issues</Link> : null}
       <Routes>
         <Route path="/" element={<Issues />} />
         <Route path="/add" element={<AddIssue />} />
