2c65fc989cd80072700e8c09a882b6607eab8ea7
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-08-29 21:42:25 +0100

    refactor(useIssuesSearch): only searches when provided is not falsey and improved API

--- src/hooks/issues/useIssuesSearch.ts
+++ src/hooks/issues/useIssuesSearch.ts
@@ -1,14 +1,14 @@
 import { useQuery } from "react-query"
 import { toDomainIssue } from "./toDomainIssue"
 
-export function useIssuesSearch({ titleQuery }: { titleQuery: string }) {
-  const keys = ["search", "issues", { query: titleQuery }]
+export function useIssuesSearch(searchValue: string) {
+  const keys = ["issues", "search", searchValue]
 
   function fetcher(): Promise<Issue[]> {
-    return fetch(`/api/search/issues?q=${titleQuery}`)
+    return fetch(`/api/search/issues?q=${searchValue}`)
       .then((res) => res.json())
       .then((data) => data.map((dto: IssueDto) => toDomainIssue(dto)))
   }
 
-  return useQuery(keys, fetcher)
+  return useQuery(keys, fetcher, { enabled: !!searchValue })
 }
