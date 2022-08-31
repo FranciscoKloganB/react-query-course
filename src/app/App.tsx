82dadf10d361d61556e0dddc7d4a5ea26b8d6ba1
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-08-31 08:01:43 +0100

    feat: indicate fetches are occuring globally

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
 import { AuthenticatedApp } from "@app/AuthenticatedApp"
 import { UnauthenticatedApp } from "@app/UnauthenticatedApp"
 import { Heading } from "@styled"
+import { useIsFetching } from "@tanstack/react-query"
+import clsx from "clsx"
 
 const useAuth = () => {
   const user = "john doe"
@@ -14,9 +16,16 @@ const useAuth = () => {
 
 function App() {
   const { isAuthenticated } = useAuth()
+  const fetchingCount = useIsFetching()
 
   return (
-    <div className="max-w-screen min-h-screen border-t-8 border-yellow-400 bg-slate-900">
+    <div className="max-w-screen min-h-screen bg-slate-900">
+      <div
+        className={clsx(
+          "sticky top-0 z-50 border-t-8 border-yellow-400",
+          fetchingCount > 0 && "animate-pulse"
+        )}
+      />
       <div className="container mx-auto px-4 py-3 lg:pt-6 xl:px-14 2xl:px-16">
         <Heading className="mb-2 lg:mb-6">Issue Tracker</Heading>
         {isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />}
