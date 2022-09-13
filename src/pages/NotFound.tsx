2680def37ba6e17da2bc0998be7d191eebadc153
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-09-13 22:13:36 +0100

    feat: add NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import { Title, Subtitle } from "@styled"
+
+export default function NotFound() {
+  return (
+    <main className="grid justify-center text-center">
+      <div>
+        <Title className="">Oops. There is nothing here!</Title>
+      </div>
+      <Link to="/issues">
+        <Subtitle className="inline-flex underline hover:text-yellow-400">
+          See all Issues
+        </Subtitle>
+      </Link>
+    </main>
+  )
+}
