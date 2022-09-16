51ea3bd946d6c81834968004f2c4157fcd18affc
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-09-16 09:11:11 +0100

    feat: add Homebreadcrumb constant component

--- src/common/routes/breadcrumbs.tsx
+++ src/common/routes/breadcrumbs.tsx
@@ -1,5 +1,13 @@
+import { GoHome } from "react-icons/go"
 import type { BreadcrumbComponentProps } from "use-react-router-breadcrumbs"
 
 export const DynamicIssueDetailBreadcrumb = (p: BreadcrumbComponentProps) => (
   <span>Issue #{p.match.params.number}</span>
+)
+
+export const HomeBreadcrumb = (
+  <>
+    <GoHome className="h-3.5 w-3.5 flex-shrink-0" aria-hidden="true" />
+    <span className="sr-only">Home</span>
+  </>
 )
