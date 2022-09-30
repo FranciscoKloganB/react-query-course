9168e71bb832f478db983002ad7d5b61a5c25803
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-09-30 23:46:28 +0100

    feat: add useUsersList hook

---
+++ src/hooks/users/useUsersList.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query"
+
+import { baseClient } from "@clients"
+import { QKF } from "@common/query-key.factory"
+
+function fetchUserList(signal?: AbortSignal) {
+  return baseClient<User>(`/api/users`, { signal })
+}
+
+function useUsersList() {
+  return useQuery(QKF.users, ({ signal }) => fetchUserList(signal))
+}
+
+export { fetchUserList, useUsersList }
