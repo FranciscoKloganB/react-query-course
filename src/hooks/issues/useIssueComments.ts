ab8db4bb1d108be6f6068c690fabe2b5b3f96e96
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-10-18 09:56:43 +0100

    refactor(useIssueComments): nnow uses useInfiniteQuery

--- src/hooks/issues/useIssueComments.ts
+++ src/hooks/issues/useIssueComments.ts
@@ -1,15 +1,40 @@
-import { useQuery } from "@tanstack/react-query"
+import { useInfiniteQuery } from "@tanstack/react-query"
 
 import { baseClient } from "@clients"
 import { QKF } from "@common/query-key.factory"
 
-function fetchIssueComments(number: string | number, signal?: AbortSignal) {
-  return baseClient<UserComment[]>(`/api/issues/${number}/comments`, { signal })
+function fetchIssueComments(
+  number: string | number,
+  pageParam: number,
+  signal?: AbortSignal
+) {
+  return baseClient<UserComment[]>(
+    `/api/issues/${number}/comments?page=${pageParam}`,
+    { signal }
+  )
 }
 
 function useIssueComments(number: string | number) {
-  return useQuery(QKF.issueComments(number), ({ signal }) =>
-    fetchIssueComments(number, signal)
+  return useInfiniteQuery(
+    QKF.issueComments(number),
+    ({ signal, pageParam = 1 }) =>
+      fetchIssueComments(number, pageParam, signal),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (lastPage.length === 0) {
+          return
+        }
+
+        return pages.length + 1
+      },
+      getPreviousPageParam: (lastPage, pages) => {
+        if (lastPage.length === 0) {
+          return
+        }
+
+        return pages.length - 1
+      }
+    }
   )
 }
 
